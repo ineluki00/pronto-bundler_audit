05ebe3d633db9a25d7afd113140bf2e7de4ebf9a
lbarre <lilian.barre@lorcyber.eu>
2021-03-05 11:26:00 +0100

    [fix] pronto 0.11.0 compatibility with gitlab

---
+++ lib/formatter/gitlab_merge_request_review_formatter.rb
@@ -0,0 +1,26 @@
+module Pronto
+  module Formatter
+    # Pronto::Formatter::GitlabMergeRequestReviewFormatter comes from the
+    # Pronto gem itself.
+    #
+    # The methods below are feature overrides to:
+    #   1. prevent the {#line_number} class from failing if none of the patches
+    #      contain the `message.line.line.new_lineno` value found. Which can happen
+    #      in the context of this pronto-bundler audit gem since we aren't
+    #      necessarily altering the Gemfile.lock file within a PR at the time of
+    #      finding an issue in the Gemfile.lock file.
+    class GitlabMergeRequestReviewFormatter
+      def line_number(message, _)
+        if message.line
+          if defined?(message.line.line)
+            message.line.line.new_lineno
+          elsif defined?(message.line.commit_line)
+            message.line.commit_line.new_lineno
+          else
+            message.line.new_lineno
+          end
+        end
+      end
+    end
+  end
+end
